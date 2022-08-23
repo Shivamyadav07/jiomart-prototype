a51b86933d2cc9c182d4d81e68818e76b3c3f611
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-08-23 15:41:54 +0530

    Update index.js

--- jiomart-prototype/src/index.js
+++ jiomart-prototype/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
 
 
 
