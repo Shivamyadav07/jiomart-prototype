c44a33bdd8c9f65e57de6ec5db71bab459be28a4
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-08-27 09:55:46 +0530

    Update App.js

--- jiomart-prototype/src/App.js
+++ jiomart-prototype/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-// import SearchDisplay from './HomePage/Search/SearchDisplay';
 import MainRoutes from './Pages/MainRoutes';
-
 
 function App() {
   return (
     <div >
-      <MainRoutes/>
-      
-     {/* <SearchDisplay/> */}
-     
+      <MainRoutes/>           
     </div>
   );
 }
