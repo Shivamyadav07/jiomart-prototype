5f14c71e00daabb763138717e63d01ecd2dd3961
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-08-28 02:22:21 +0530

    sorting.jsx update

--- jiomart-prototype/src/components/ProducPage/ProductPageJsx/MultipleProductsSection.jsx
+++ jiomart-prototype/src/components/ProducPage/ProductPageJsx/MultipleProductsSection.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import styles from '../ProductPageModuleCss/MultipleProductsSection.module.css'
 import { Pagination } from './Pagination'
 import { SingleProductCard } from './SingleProductCard'
+import { SortingDiv } from './SortingDiv'
 
-export const MultipleProductsSection = ({data}) => {
+export const MultipleProductsSection = ({ data }) => {
   return (
     <div className={styles.maindiv}>
+      <SortingDiv />
       <div className={styles.title}>
         ALL PRODUCTS
       </div>
       <div className={styles.productdiv}>
         {
-          data.map((e)=>{
-            return <SingleProductCard key={e.id} item={e} />
+          data.map((e) => {
+            return <SingleProductCard item={e} />
           })
         }
       </div>
-      <Pagination/>
+      <Pagination />
     </div>
   )
 }
