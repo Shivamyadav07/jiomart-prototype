1a6d9b3a5050c69fe20e8a17ce69a6940be47a0e
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-08-27 17:23:31 +0530

    added product description page

--- jiomart-prototype/src/Pages/MainRoutes.jsx
+++ jiomart-prototype/src/Pages/MainRoutes.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { DescriptionPage } from "../components/DescriptionPage/DescriptionPage";
 import MainHome from "../HomePage/MainHome";
 import CartPage from "./CartPage";
 import OrderSummaryPage from "./OrderSummaryPage";
+import { Beauty } from "./ProductPages/Beauty";
+import { Electronics } from "./ProductPages/Electronics";
+import { Fashion } from "./ProductPages/Fashion";
+import { Groceries } from "./ProductPages/Groceries";
+import { HomeKitchen } from "./ProductPages/HomeKitchen";
+import { Jewellery } from "./ProductPages/Jewellery";
+import { PremiumFruits } from "./ProductPages/PremiumFruits";
 
 const MainRoutes = () => {
   return (
@@ -10,6 +18,14 @@ const MainRoutes = () => {
       <Route path="/" element={<MainHome />}></Route>
       <Route path="/cart" element={<CartPage />}></Route>
       <Route path="/order" element={<OrderSummaryPage />}></Route>
+      <Route path="/beauty" element={<Beauty/>}></Route>
+      <Route path="/electronics" element={<Electronics/>}></Route>
+      <Route path="/fashion" element={<Fashion/>}></Route>
+      <Route path="/homeKitchen" element={<HomeKitchen/>}></Route>
+      <Route path="/jewellery" element={<Jewellery/>}></Route>
+      <Route path="/groceries" element={<Groceries/>}></Route>
+      <Route path="/premiumfruits" element={<PremiumFruits/>}></Route>
+      <Route path="/product/:id" element={<DescriptionPage/>}></Route>
     </Routes>
   );
 };
--- jiomart-prototype/src/components/ProducPage/ProductPageJsx/SingleProductCard.jsx
+++ jiomart-prototype/src/components/ProducPage/ProductPageJsx/SingleProductCard.jsx
@@ -16,8 +16,8 @@ export const SingleProductCard = ({item}) => {
                 <img className={styles.discountimg} src="https://i.ibb.co/Yd3ZVDg/badge.png" alt="badge" border="0" />
                 
             </div>
-        <img className={styles.productimage} src={item.image} alt='Productimage' />
-            <p className={styles.heading}>Jiomart Blue and White Polyester 2 Fold Umbrella 62.2 cm</p>
+        <img className={styles.productimage} src={item.image} alt='Productimage'/>
+            <p className={styles.heading}>{item.title}</p>
 
 
             <p className={styles.mrpdiv}>M.R.P:<> </></p>
