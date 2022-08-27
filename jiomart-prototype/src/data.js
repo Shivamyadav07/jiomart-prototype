fab415107d189510b435bbc8a233f937ee91773e
singhankita29 <100141535+singhankita29@users.noreply.github.com>
2022-08-27 11:17:16 +0530

    added more section

--- jiomart-prototype/src/data.js
+++ jiomart-prototype/src/data.js
@@ -178,3 +178,129 @@ export const HottestDeals=[
         image:"https://www.jiomart.com/images/cms/section/category/501/525.png?v=1628791106"
     }
 ]
+
+
+
+export const groceries=[
+{
+    image:"https://www.jiomart.com/images/category/188/thumb/breakfast-cereals-20200603.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/117/thumb/dairy-20200522.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/17/thumb/0-17.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/20/thumb/0-20.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/119/thumb/bath-hand-wash-20200714.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/7/thumb/toothpaste-20200520.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/26/thumb/0-26.png",
+},
+{
+    image:"https://www.jiomart.com/images/category/213/thumb/paper-tissues-napkins-20210727.png",
+}
+]
+
+export const GrocerieSlider=[
+    {
+        imgSrc: "https://www.jiomart.com/images/product/150x150/592200116/saffola-gold-refined-rice-bran-and-sunflower-based-blended-oil-3-l-daawat-devaaya-basmati-rice-5-kg-combo-pack-0-20220624.jpg",
+        heading:"Saffola Gold Refined Rice Bran And Sunflower Based Blended Oil 3 L + Daawat Devaaya Basmati Rice 5 kg (Combo Pack) ",
+        price: "₹ 875.00",
+        mrp: "M.R.P:",
+        rupees:"₹ 1313",
+        save: "₹ 435.00",
+    },
+    {
+        imgSrc: "https://www.jiomart.com/images/product/600x600/491055460/colgate-strong-teeth-dental-cream-toothpaste-500-g-saver-pack-product-images-o491055460-p491055460-0-202203151056.jpg",
+        heading:"Colgate Strong Teeth Dental Cream Toothpaste 500 g Saver Pack",
+        price: "₹ 188.00",
+        mrp: "M.R.P:",
+        rupees:"₹ 253",
+        save: "₹ 65.00",
+    },
+    {
+        imgSrc: "https://www.jiomart.com/images/product/150x150/492367966/surf-excel-easy-wash-detergent-powder-5-kg-product-images-o492367966-p590837659-0-202208111657.jpg",
+        heading:"Surf Excel Easy Wash Detergent Powder 5 kg",
+        price: "₹ 678.00",
+        mrp: "M.R.P:",
+        rupees:"₹ 730",
+        save: "₹ 52.00",
+    },
+    {
+        imgSrc: "https://www.jiomart.com/images/product/150x150/490003834/maggi-2-minute-masala-instant-noodles-560-g-product-images-o490003834-p490003834-0-202203150702.jpg",
+        heading:"Maggi 2-Minute Masala Instant Noodles 560 g",
+        price: "₹ 92.00",
+        mrp: "M.R.P:",
+        rupees:"₹ 102",
+        save: "₹ 17.00",
+    },
+    {
+        imgSrc: "https://www.jiomart.com/images/product/150x150/490001392/amul-butter-500-g-carton-product-images-o490001392-p490001392-0-202203152128.jpg",
+        heading:"Amul Butter 500 g (Cartoon)",
+        price: "₹ 245.00",
+        mrp: "M.R.P:",
+        rupees:"₹ 255",
+        save: "₹ 10.00",
+    },
+    {   imgSrc:"https://www.jiomart.com/images/product/150x150/490001392/amul-butter-500-g-carton-product-images-o490001392-p490001392-0-202203152128.jpg",
+        heading:"Tul / Arhar Dal 2 Kg",
+        price: "₹ 252.00",
+        mrp: "M.R.P:",
+        rupees:"₹ 300",
+        save: "₹ 48.00",
+    },
+    // {
+    //     imgSrc: "https://www.jiomart.com/images/product/150x150/490008332/closeup-everfresh-red-hot-gel-toothpaste-150-g-pack-of-2-product-images-o490008332-p490008332-0-202206222347.jpg",
+    //     heading:"Closeup Everfresh+ Red Hot Gel Toothpaste 150 g (Pack of 2)",
+    //     price: "₹ 163.00",
+    //     mrp: "M.R.P:",
+    //     rupees:"₹ 182",
+    //     save: "₹ 19.00",
+    // },
+    // {
+    //     imgSrc: "https://www.jiomart.com/images/product/150x150/492848164/surf-excel-matic-front-load-detergent-powder-6-kg-product-images-o492848164-p591223797-0-202204291101.jpg",
+    //     heading:"Surf Excel Matic Front Load Detergent Powder 6 kg",
+    //     price: "₹ 1325.00",
+    //     mrp: "M.R.P:",
+    //     rupees:"₹ 1425",
+    //     save: "₹ 100.00",
+    // },
+    // {
+    //     imgSrc: "https://www.jiomart.com/images/product/150x150/492391349/kissan-fresh-tomato-ketchup-1-2-kg-product-images-o492391349-p590809928-0-202203150627.jpg",
+    //     heading:"Kissan Fresh Tomato Ketchup 1.2 Kg",
+    //     price: "₹ 125.00",
+    //     mrp: "M.R.P:",
+    //     rupees:"₹ 160",
+    //     save: "₹ 35.00",
+    // }, {
+    //     imgSrc: "https://www.jiomart.com/images/product/150x150/490010244/gowardhan-pure-cow-ghee-1-l-pouch-product-images-o490010244-p490010244-0-202203150918.jpg",
+    //     heading:"Govardhan Pure Ghee 1Litre (Pouch)",
+    //     price: "₹ 566.00",
+    //     mrp: "M.R.P:",
+    //     rupees:"₹ 609",
+    //     save: "₹ 43.00",
+    // }, {
+    //     imgSrc: "https://www.jiomart.com/images/product/150x150/491252835/tide-jasmine-rose-detergent-powder-5-kg-get-extra-1-kg-free-product-images-o491252835-p590838653-0-202203151949.jpg",
+    //     heading:"Tide Jasmine & Rose Detergent Powder 5 Kg",
+    //     price: "₹ 637.00",
+    //     mrp: "M.R.P:",
+    //     rupees:"₹ 635",
+    //     save: "₹ 48.00",
+    // }, {
+    //     imgSrc: "https://www.jiomart.com/images/product/150x150/491319789/sunfeast-dark-fantasy-original-choco-filled-biscuits-300-g-product-images-o491319789-p491319789-0-202205301919.jpg",
+    //     heading:"Sunfeast Dark Fantasy Original Choco Filled",
+    //     price: "₹ 99.00",
+    //     mrp: "M.R.P:",
+    //     rupees:"₹ 170",
+    //     save: "₹ 71.00",
+    // }
+    
+    
+]
