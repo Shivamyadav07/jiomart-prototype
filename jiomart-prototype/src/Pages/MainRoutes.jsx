254adf96934eca928b88d492d6f97f8b36a23cb4
myronpereira3 <myronpereira3@gmail.com>
2022-08-27 13:01:01 +0530

    day5

--- jiomart-prototype/src/Pages/MainRoutes.jsx
+++ jiomart-prototype/src/Pages/MainRoutes.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainHome from "../HomePage/MainHome";
 import CartPage from "./CartPage";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainHome />}></Route>
       <Route path="/cart" element={<CartPage />}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/login" element={<Login/>}></Route> 
     </Routes>
   );
 };
