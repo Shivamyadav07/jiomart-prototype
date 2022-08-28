07bc9eab689f8eb7d4f9956d8d07f907d14abb38
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-08-28 13:57:04 +0530

    update product page

--- jiomart-prototype/src/components/ProducPage/ProductPageJsx/MultipleProductsSection.jsx
+++ jiomart-prototype/src/components/ProducPage/ProductPageJsx/MultipleProductsSection.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import styles from '../ProductPageModuleCss/MultipleProductsSection.module.css'
 import { Pagination } from './Pagination'
 import { SingleProductCard } from './SingleProductCard'
-import { SortingDiv } from './SortingDiv'
 
 export const MultipleProductsSection = ({ data }) => {
   return (
     <div className={styles.maindiv}>
-      <SortingDiv />
       <div className={styles.title}>
         ALL PRODUCTS
       </div>
--- jiomart-prototype/src/components/ProducPage/ProductPageJsx/ProductPage.jsx
+++ jiomart-prototype/src/components/ProducPage/ProductPageJsx/ProductPage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { MultipleProductsSection } from './MultipleProductsSection'
 import { ProductPagePoster } from './ProductPagePoster'
 import styles from '../ProductPageModuleCss/ProductPage.module.css'
+import { SortingDiv } from './SortingDiv'
 
 export const ProductPage = ({poster,data}) => {
   return (
     <div className={styles.maindiv}>
         <ProductPagePoster poster={poster}/>
+        <SortingDiv />
         <MultipleProductsSection data={data} />
     </div>
   )
