b60d40dfbbc549425255fc5dd641b312e2e62234
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-08-28 18:03:33 +0530

    Update action.js

--- jiomart-prototype/src/Redux/CartReducer/action.js
+++ jiomart-prototype/src/Redux/CartReducer/action.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const getCartData = () => (dispatch) => {
   dispatch({ type: types.GET_CARTDATA_REQUEST });
   return axios
-    .get("http://shivam-yadav-json-server-data.herokuapp.com/Cart")
+    .get("https://shivam-yadav-json-server-data.herokuapp.com/Cart")
     .then((res) => {
       console.log("GetRes:", res);
       return dispatch({ type: types.GET_CARTDATA_SUCCESS, payload: res.data });
@@ -17,7 +17,7 @@ export const getCartData = () => (dispatch) => {
 export const addCartData = (payload) => (dispatch) => {
   dispatch({ type: types.ADD_CARTDATA_REQUEST });
   return axios
-    .post(`http://shivam-yadav-json-server-data.herokuapp.com/Cart`, payload)
+    .post(`https://shivam-yadav-json-server-data.herokuapp.com/Cart`, payload)
     .then((res) => {
       console.log("AddRes:", res);
       return dispatch({ type: types.ADD_CARTDATA_SUCCESS, payload: res.data });
@@ -32,13 +32,13 @@ export const updateCartData = (id, payload) => (dispatch) => {
   dispatch({ type: types.UPDATE_CARTDATA_REQUEST });
   return axios
     .patch(
-      `http://shivam-yadav-json-server-data.herokuapp.com/Cart/${id}`,
+      `https://shivam-yadav-json-server-data.herokuapp.com/Cart/${id}`,
       payload
     )
     .then((res) => {
       console.log("UpdateRes:", res);
       axios
-        .get("http://shivam-yadav-json-server-data.herokuapp.com/Cart")
+        .get("https://shivam-yadav-json-server-data.herokuapp.com/Cart")
         .then((res) => {
           console.log("InnerRes:", res);
           return dispatch({
@@ -57,13 +57,13 @@ export const deleteCartData = (payload) => (dispatch) => {
   dispatch({ type: types.DELETE_CARTDATA_REQUEST });
   return axios
     .delete(
-      `http://shivam-yadav-json-server-data.herokuapp.com/Cart/${payload.id}`,
+      `https://shivam-yadav-json-server-data.herokuapp.com/Cart/${payload.id}`,
       payload
     )
     .then((res) => {
       console.log("deleteRes:", res);
       axios
-        .get("http://shivam-yadav-json-server-data.herokuapp.com/Cart")
+        .get("https://shivam-yadav-json-server-data.herokuapp.com/Cart")
         .then((res) => {
           return dispatch({
             type: types.DELETE_CARTDATA_SUCCESS,
