54ac4aa487d8444a9703cef2e50528fac6a69577
Hitesh Jamunkar <101565807+HiteshJamunkar@users.noreply.github.com>
2022-08-28 13:06:04 +0530

    Update Checkout.jsx

--- jiomart-prototype/src/Pages/Checkout.jsx
+++ jiomart-prototype/src/Pages/Checkout.jsx
@@ -4,7 +4,7 @@ import { Radio, Spin, Alert } from 'antd';
 import './Checkout.css';
 import CartPage from './CartPage';
 import { useHistory } from 'react-router';
-import { updateCart } from '../../Redux/actions';
+import { updateCart } from '../../Redux/action';
 const Checkout = ({ match, location, history }) => {
   const isAuth = useSelector((store) => store.Auth.isAuth);
   const dispatch = useDispatch();
